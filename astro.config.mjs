c474f3d2cb6137e8be219b421c8f0e864af42fba
Mitchell <68230846+Creaous@users.noreply.github.com>
2024-08-12 19:53:51 +0930

    add new redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	redirects: {
-		"/plugin-keycloak": "https://github.com/Nexirift/plugin-keycloak",
-		"/plugin-keycloak-example": "https://github.com/Nexirift/plugin-keycloak-example",
-		"/spark": "https://github.com/Nexirift/spark"
-	},
+  redirects: {
+    "/plugin-keycloak": "https://github.com/Nexirift/plugin-keycloak",
+    "/plugin-keycloak-example":
+      "https://github.com/Nexirift/plugin-keycloak-example",
+    "/plugin-oidc": "https://github.com/Nexirift/plugin-oidc",
+    "/plugin-oidc-example": "https://github.com/Nexirift/plugin-oidc-example",
+    "/spark": "https://github.com/Nexirift/spark",
+  },
 });
